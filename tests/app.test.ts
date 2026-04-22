381efbe9c91aafb3b4cdba26a178ffaf1d4f9fde
Vegard <vegard@Vegards-MacBook-Pro.local>
2026-04-22 23:47:18 +0200

    v0.2.3 First test - Testing that the basic setup of the app works

---
+++ tests/app.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest';
+import { app } from '../src/app';
+
+describe('General Server Tests', () => {
+
+    it('should return 200 OK for the home (login) page', async () => {
+        const response = await request(app).get('/');
+
+        expect(response.status).toBe(200);
+        expect(response.text).toContain('Welcome to DexMatch');
+    });
+
+    it('should return 404 for a route that does not exist', async () => {
+        const response = await request(app).get('/this-is-not-a-real-page');
+        expect(response.status).toBe(404);
+    });
+});
