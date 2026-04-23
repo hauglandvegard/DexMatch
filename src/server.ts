dbcc42e890bc75f7afeabcffa9961e9890f397f1
Vegard <vegard@Vegards-MacBook-Pro.local>
2026-04-23 19:19:53 +0200

    Fixed logging error in server.ts

--- src/server.ts
+++ src/server.ts
@@ -6,5 +6,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     logger.info(`DexMatch is live at http://localhost:${PORT}`);
     logger.debug(`Development proxy at http://localhost:3001`);
-    console.log(`Press Ctrl+C to stop`);
+    logger.info(`Press Ctrl+C to stop`);
 });
