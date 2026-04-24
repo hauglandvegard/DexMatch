2d8e4dcd1826b0aba2d7d91bf013cfa91e8ebebd
Vegard <vegard@Vegards-MacBook-Pro.local>
2026-04-25 01:56:55 +0200

    Feat: Generate individual values for Pokémon
    Implement logic to generate individual values (IVs) for Pokémon based on
    Generation III rules. Legendary Pokémon receive three automatic 31 IVs.

--- src/services/pokeGenerator.ts
+++ src/services/pokeGenerator.ts
@@ -26,9 +26,11 @@
 import { faker } from "@faker-js/faker";
 
 import { getBellCurveRandom } from "../utils/mathUtils";
+import { getRandomIndices } from "../utils/arrayUtils";
 import {
     Pokemon,
     PokeAttributes,
+    PokeStats,
     CleanSpeciesData,
 } from "../types/pokemon.types";
 import logger from "../utils/logger";
@@ -55,6 +57,41 @@ function randomizeSize(attr: PokeAttributes): PokeAttributes {
     return attr;
 }
 
+/**
+ * Genaretes individual values for a pokémon that determines it´s individual stats according to
+ * Genartion III logic (https://bulbapedia.bulbagarden.net/wiki/Individual_values).
+ * @returns A PokemonIndividualValues object.
+ */
+function generatePokemonIVs(isLegendary: boolean): PokeStats {
+    const ivs = [];
+
+    // If the pokemon is legendary, 3 of the 6 values are automatically 31
+    const randomIdxs = isLegendary ? getRandomIndices(6, 3) : [];
+    let curRandIdx = 0;
+    let stat = 0;
+
+    // Individual stats are given on a value from 0 to 31
+    for (let i = 0; i < 6; i++) {
+        if (isLegendary && i === randomIdxs[curRandIdx]) {
+            stat = 31;
+            curRandIdx += 1;
+        } else {
+            stat = Math.floor(Math.random() * 32);
+        }
+
+        ivs.push(stat);
+    }
+
+    return {
+        hp: ivs[0],
+        atk: ivs[1],
+        def: ivs[2],
+        spAtk: ivs[3],
+        spDef: ivs[4],
+        speed: ivs[5],
+    };
+}
+
 export function generatePokemon(speciesData: CleanSpeciesData): Pokemon {
     const attr = randomizeSize(speciesData.attributs);
 
@@ -65,7 +102,7 @@ export function generatePokemon(speciesData: CleanSpeciesData): Pokemon {
         description: "", // TODO: Set up the Chuck Norries joke import
         level: 1, // TODO: Implement function to calculate level
         attributes: attr,
-        statsIV: speciesData.stats, // TODO: Calculate IVs
+        statsIV: generatePokemonIVs(speciesData.isLegendary),
         isShiny: false, // TODO: Implement function to calculate if shiny
     };
 
