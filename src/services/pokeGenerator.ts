54c7c623ddefb89c77386241e3bbae4fc7949271
Vegard <vegard@Vegards-MacBook-Pro.local>
2026-04-25 01:27:47 +0200

    Added func to pokeGenerator.ts - Added rendomSize()

--- src/services/pokeGenerator.ts
+++ src/services/pokeGenerator.ts
@@ -25,17 +25,46 @@
 
 import { faker } from "@faker-js/faker";
 
-import { Pokemon, CleanSpeciesData } from "../types/pokemon.types";
+import { getBellCurveRandom } from "../utils/mathUtils";
+import {
+    Pokemon,
+    PokeAttributes,
+    CleanSpeciesData,
+} from "../types/pokemon.types";
 import logger from "../utils/logger";
 
+/**
+ * Gives each pokémon a bit more uniqueness by randomizing their size.
+ * * @param attr - The pokémon's attributes to be modified
+ * @returns A modified version of the attributes
+ */
+function randomizeSize(attr: PokeAttributes): PokeAttributes {
+    const sizeGene = getBellCurveRandom();
+
+    // Weight variance: 0.6x to 1.4x
+    const weightMultiplier = sizeGene * (1.4 - 0.6) + 0.6;
+
+    attr.weight = Math.floor(attr.weight * weightMultiplier);
+
+    // Height variance: 0.8x to 1.2x (with a +/- 10% wobble)
+    const heightWobble = Math.random() * 0.2 - 0.1;
+    const heightMultiplier = sizeGene * (1.2 - 0.8) + 0.8 + heightWobble;
+
+    attr.height = Math.floor(attr.height * heightMultiplier);
+
+    return attr;
+}
+
 export function generatePokemon(speciesData: CleanSpeciesData): Pokemon {
+    const attr = randomizeSize(speciesData.attributs);
+
     const pokemon: Pokemon = {
         id: 0, // TODO: Set up call to database to get a unique id
         name: faker.person.firstName(), // REQUIREMENT: Each pokémon should have a random human name. I.e. Josh the Charmander
         speciesId: speciesData.id,
         description: "", // TODO: Set up the Chuck Norries joke import
         level: 1, // TODO: Implement function to calculate level
-        attributes: speciesData.attributs, // TODO: Calculate attributes
+        attributes: attr,
         statsIV: speciesData.stats, // TODO: Calculate IVs
         isShiny: false, // TODO: Implement function to calculate if shiny
     };
