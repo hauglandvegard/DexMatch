1939c61d619f1d0c6454cb461a4d7811f20b7d4e
Vegard <vegard@Vegards-MacBook-Pro.local>
2026-04-26 01:30:26 +0200

    Add function to generate and save Pokemon

--- src/services/pokeService.ts
+++ src/services/pokeService.ts
@@ -1,3 +1,34 @@
-/*
-    The connection point that connects all the logic regarding pokémon
-*/
+import { buildCleanSpeciesData } from './pokeApi.service';
+import getJokeForType from './CNService';
+import generatePokemon from './pokeGenerator';
+import { insertPokemon } from '../models/Pokemon';
+import { Pokemon } from '../types/pokemon.types';
+
+const MAX_SPECIES_ID = 898;
+
+export interface PokemonProfile {
+    pokemon: Pokemon;
+    speciesName: string;
+    spriteUrl: string;
+    shinySpriteUrl: string;
+}
+
+export async function getNextPokemon(): Promise<PokemonProfile> {
+    const speciesId = Math.floor(Math.random() * MAX_SPECIES_ID) + 1;
+
+    const speciesData = await buildCleanSpeciesData(speciesId);
+    if (!speciesData) throw new Error(`Failed to fetch data for species ${speciesId}`);
+
+    const primaryType = speciesData.types[0] ?? 'normal';
+    const joke = await getJokeForType(primaryType);
+
+    const draft = generatePokemon(speciesData, joke);
+    const pokemon = insertPokemon(draft);
+
+    return {
+        pokemon,
+        speciesName: speciesData.name,
+        spriteUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${speciesId}.png`,
+        shinySpriteUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${speciesId}.png`,
+    };
+}
